b36142cafad4e7f3ee3263bbd4ac46024bc7bf36
Varun <40390368+tips4you@users.noreply.github.com>
2024-09-12 16:09:28 -0400

    fix

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,7 +7,6 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../'
 
 from main import app
 
-
 def test_homepage():
     # Create a test client using Flask's app
     client = app.test_client()
@@ -20,3 +19,4 @@ def test_homepage():
 
     # Check if the name in the response is correct
     assert b"Hello PathwayTips!" in response.data
+
