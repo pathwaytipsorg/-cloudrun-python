623881b7911d02fb93fdb5a0a12d0a207161ba91
Varun <40390368+tips4you@users.noreply.github.com>
2024-09-12 15:56:54 -0400

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,12 @@
 # tests/test_main.py
+import sys
+import os
+
+# Add the project directory to the sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 
 from main import app
+
 
 def test_homepage():
     # Create a test client using Flask's app
